a1f7479cfd3b8a8c7a54febec7ad899925084252
sumit <sumitmondal9448@gmail.com>
2023-01-08 20:16:43 +0530

    change newGame

--- src/components/newGame/NewGame.jsx
+++ src/components/newGame/NewGame.jsx
@@ -1,11 +1,13 @@
 import React from "react";
+import { useHistory } from "react-router-dom";
 import Header from "../header/Header";
 import "./newGame.css";
 const NewGame = () => {
+  const history = useHistory();
   return (
     <div className="newGame">
       <h1>No Games Found</h1>
-      <button>Start a new Game</button>
+      <button onClick={() => history.push("/newGame")}>Start a new Game</button>
     </div>
   );
 };
