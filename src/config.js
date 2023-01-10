bd5307a23944e7d9818553e275c29e76764dbcda
sumit <sumitmondal9448@gmail.com>
2023-01-10 10:14:55 +0530

    chanelog

--- src/config.js
+++ src/config.js
@@ -1,7 +1,5 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: "http://localhost:8000/connect"
-    ? "http://localhost:8000/connect"
-    : "https://tictactoebaackend.onrender.com/connect",
+  baseURL: "https://tictactoebaackend.onrender.com/connect",
 });
