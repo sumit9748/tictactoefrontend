b1e3a226ea27f60aa202f9416e151b9c02a5e9c2
sumit <sumitmondal9448@gmail.com>
2023-02-18 23:09:13 +0530

    change url

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: "https://tictactoesumit.onrender.com/connect",
+  baseURL: "https://tictactoesumit.onrender.com",
 });
